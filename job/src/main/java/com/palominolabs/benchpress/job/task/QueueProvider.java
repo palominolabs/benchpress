a3d550f1e20813966652e5f6058d3e003facfc56
Marshall Pierce <marshall@palominolabs.com>
2013-06-06 17:01:36 -0700

    Add some javadoc

--- job/src/main/java/com/palominolabs/benchpress/job/task/QueueProvider.java
+++ job/src/main/java/com/palominolabs/benchpress/job/task/QueueProvider.java
@@ -5,16 +5,28 @@ import javax.annotation.concurrent.ThreadSafe;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * TaskFactory implementations may optionally use the provided queue to defer processing implementation-specific data.
+ * This data is processed by another thread, the specifics of which are defined by the implementation of QueueProvider.
+ * The data processing is done by whatever the appropriate ComponentFactory returns for {@link
+ * ComponentFactory#getTaskOutputProcessor()}.
+ */
 @ThreadSafe
 public interface QueueProvider {
     /**
-     * Provider for creating a queue for each runnable. Do not reuse the result of this across task runnables.
+     * Get a queue to be used for one runnable. Do not reuse the result of this across task runnables.
+     *
+     * @param taskType the task type
+     * @param jobId    job id
      * @return A queue
-     * @param taskType
-     * @param jobId
      */
     @Nonnull
     BlockingQueue<Object> getQueue(String taskType, UUID jobId);
 
+    /**
+     * Should be called when the node is done with its partition for a given job.
+     *
+     * @param jobId job id
+     */
     void removeJob(UUID jobId);
 }
--- job/src/main/java/com/palominolabs/benchpress/job/task/TaskOutputProcessor.java
+++ job/src/main/java/com/palominolabs/benchpress/job/task/TaskOutputProcessor.java
@@ -1,5 +1,13 @@
 package com.palominolabs.benchpress.job.task;
 
+/**
+ * Plugins may choose to implement this interface and use the QueueProvider to defer processing results of tasks.
+ */
 public interface TaskOutputProcessor {
+    /**
+     * Called once per object in the queue.
+     *
+     * @param output an object provided to the output queue by a task
+     */
     void handleOutput(Object output);
 }
